4a00898e4b77c1f44aceff96d09513364ae3029d
horsjaco117 <jacob_horsley@live.com>
2025-02-06 20:28:14 -0700

    Add project files.

---
+++ FortuneCookie/Module1.vb
@@ -0,0 +1,65 @@
+﻿'Jacob Horsley
+'RCET 0265
+'Spring 2025
+'Fortune Cookie
+'URL: 
+
+Option Strict On
+Option Explicit On
+
+Module Module1
+
+    Sub Main()
+        Dim userInput As String
+
+        Console.WriteLine("Estas listo para una adivinanza?")
+        Console.ReadLine()
+        Randomize(DateTime.Now.Millisecond)
+        For i = 1 To 1
+            'Console.WriteLine(RandomNumber())
+            Console.WriteLine(RandomNumberbetween(1, 3))
+        Next
+        Console.WriteLine(StrDup(80, "*"))
+        For i = 1 To 10
+            'Console.WriteLine(RandomNumber())
+            Console.WriteLine(RandomNumberbetween(1, 4))
+        Next
+
+    End Sub
+
+    Function MyActualLuckyNumber() As Integer
+        Return 77
+    End Function
+
+    Function sumof(firstNumber As Integer, secondNumber As Integer) As Integer
+        Dim result As Integer
+
+        result = firstNumber + secondNumber
+
+        Return result
+    End Function
+
+    Function RandomNumber() As Integer
+        Dim myRandomNumber As Single
+        Randomize()
+
+        myRandomNumber = Rnd() * 10
+        myRandomNumber = Int(myRandomNumber) + 1
+
+        Return CInt(myRandomNumber)
+    End Function
+
+
+    Function RandomNumberbetween(min As Integer, max As Integer) As Integer
+        'actual code here!
+        Dim temp As Single
+        Randomize()
+        temp = Rnd()
+        temp *= max - min
+        temp += min
+        Return CInt(temp)
+    End Function
+
+
+
+End Module
